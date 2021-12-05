79b966814676de852718ee1f706de509e7f27ebe
Fazeel Bhatti <pak.tech.service@gmail.com>
2021-12-05 16:48:20 +0500

    Updates

---
+++ precache-manifest.949325f770a4babed18eea2d715c3702.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61ebcf5feeec92fc9f4d474429be389d",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "1ebc82c00f1d86e8524f",
+    "url": "/resume/static/css/2.d88a60dc.chunk.css"
+  },
+  {
+    "revision": "86dc56425ed91baf2335",
+    "url": "/resume/static/css/main.8166af42.chunk.css"
+  },
+  {
+    "revision": "1ebc82c00f1d86e8524f",
+    "url": "/resume/static/js/2.21dee50e.chunk.js"
+  },
+  {
+    "revision": "daedb6e9c1e75eb2181f2dde37df55f3",
+    "url": "/resume/static/js/2.21dee50e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86dc56425ed91baf2335",
+    "url": "/resume/static/js/main.d33e03c6.chunk.js"
+  },
+  {
+    "revision": "88d66a0ce28414bf8bd8",
+    "url": "/resume/static/js/runtime-main.df5b4bf0.js"
+  },
+  {
+    "revision": "ad3724ffcdd7effa7000c2e94dbae4de",
+    "url": "/resume/static/media/dtc.ad3724ff.png"
+  },
+  {
+    "revision": "bce57fa2c109382af637173725cd9151",
+    "url": "/resume/static/media/horizon.bce57fa2.png"
+  },
+  {
+    "revision": "4836e79cf320c6cfc8eb9c7664400068",
+    "url": "/resume/static/media/person1.4836e79c.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cv/precache-manifest.ac2f374cc8fc1cf6175b06309e7d4803.js"
+  "/resume/precache-manifest.949325f770a4babed18eea2d715c3702.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cv/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/resume/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
