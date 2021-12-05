c4e1e88fc4f5e043c7c2feab12502c5f427ab541
Fazeel Bhatti <pak.tech.service@gmail.com>
2021-12-05 16:43:09 +0500

    Updates

---
+++ precache-manifest.ac2f374cc8fc1cf6175b06309e7d4803.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4e95138e87bb96b9b1c3f0380a5752f",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "1ebc82c00f1d86e8524f",
+    "url": "/cv/static/css/2.d88a60dc.chunk.css"
+  },
+  {
+    "revision": "86dc56425ed91baf2335",
+    "url": "/cv/static/css/main.8166af42.chunk.css"
+  },
+  {
+    "revision": "1ebc82c00f1d86e8524f",
+    "url": "/cv/static/js/2.21dee50e.chunk.js"
+  },
+  {
+    "revision": "daedb6e9c1e75eb2181f2dde37df55f3",
+    "url": "/cv/static/js/2.21dee50e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86dc56425ed91baf2335",
+    "url": "/cv/static/js/main.d33e03c6.chunk.js"
+  },
+  {
+    "revision": "765dce4c482e2ab47164",
+    "url": "/cv/static/js/runtime-main.11c24cce.js"
+  },
+  {
+    "revision": "ad3724ffcdd7effa7000c2e94dbae4de",
+    "url": "/cv/static/media/dtc.ad3724ff.png"
+  },
+  {
+    "revision": "bce57fa2c109382af637173725cd9151",
+    "url": "/cv/static/media/horizon.bce57fa2.png"
+  },
+  {
+    "revision": "4836e79cf320c6cfc8eb9c7664400068",
+    "url": "/cv/static/media/person1.4836e79c.png"
+  }
+]);
